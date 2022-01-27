40ca8f2c2901fbff4364141f4d709b529291a945
Marcos Eduardo <bacalhau_1995@hotmail.com>
2022-01-27 14:08:56 -0300

    Starting StaticSiteGeneration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,21 @@
+import { GetStaticProps } from 'next'; 
 import Head from 'next/head';
 
 import React from 'react';
+
+import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
+
 import styles from './home.module.scss'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <React.Fragment>
       <Head>
@@ -16,12 +28,32 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
         <p>
           Get access to all the publications <br />
-          <span>for $9.99</span>
+          <span>for {product.amount}</span>
         </p>
+        <SubscribeButton priceId={product.priceId}/>
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </React.Fragment>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1K8OYDHftCJBaXEtL4DxRsOo')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100),
+
+  }
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, //24 hours
+  }
 }
