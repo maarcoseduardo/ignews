c020d7b6f67495d1a8c03e2189ef3e976567ed0b
Marcos Eduardo <bacalhau_1995@hotmail.com>
2022-01-27 12:16:41 -0300

    Creating Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,27 @@
 import Head from 'next/head';
 
 import React from 'react';
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <React.Fragment>
       <Head>
-        <title>Ignews</title>
+        <title>Home | ig.news</title>
       </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+        <p>
+          Get access to all the publications <br />
+          <span>for $9.99</span>
+        </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </React.Fragment>
   )
 }
