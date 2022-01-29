22a7645f4c952d92e40bb5c73704c75bbb46ec4a
Marcos Eduardo <bacalhau_1995@hotmail.com>
2022-01-29 15:30:08 -0300

    Login With Github OK

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-import { query as q } from 'faunadb'
+import { query as q } from 'faunaDB'
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
@@ -22,9 +22,25 @@ export default NextAuth({
 
       try {
         await fauna.query(
-          q.Create(
-            q.Collection('users'),
-            { data: { email } }
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
           )
         )
 
